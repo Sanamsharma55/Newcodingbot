6cbe06b6edac2653d9588bac28b97325b9aa50d4
Sanamsharma55 <146613634+Sanamsharma55@users.noreply.github.com>
2023-11-08 20:29:18 +0530

    arithmetic.c
    Signed-off-by: Sanamsharma55 <146613634+Sanamsharma55@users.noreply.github.com>

---
+++ arithmetic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,sum,sub,mult,div;
+    printf("enter the first number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d",&b);
+    sum=a+b;
+    sub=a-b;
+    div=a/b;
+    mult=a*b;
+    printf("\n the sum is %d",sum);
+    printf("\n the subtraction is %d",sub);
+    printf("\n the division is %d",div);
+    printf("\n the multiplicatin is %d",mult);
+    
+}
